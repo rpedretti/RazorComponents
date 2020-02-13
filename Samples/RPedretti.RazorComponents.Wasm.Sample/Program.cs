e45c374d2460f26ad6e5991686a851e23cdf7f53
Rafael Pedretti <rafael.pedretti@agoda.com>
2020-02-13 15:20:32 +0700

    Updated blazor sample to 3.2

--- Samples/RPedretti.RazorComponents.Wasm.Sample/Program.cs
+++ Samples/RPedretti.RazorComponents.Wasm.Sample/Program.cs
@@ -1,4 +1,14 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using RPedretti.RazorComponents.Sample.Shared.HttpClients;
+using RPedretti.RazorComponents.Sample.Shared.Services;
+using RPedretti.RazorComponents.Wasm.Sample.HttpClients;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace RPedretti.RazorComponents.Wasm.Sample
 {
@@ -6,14 +16,38 @@ namespace RPedretti.RazorComponents.Wasm.Sample
     {
         #region Methods
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            RegisterDependencies(builder.Services, builder.Configuration);
+
+            builder.RootComponents.Add<App>("app");
+
+            var host = builder.Build();
+
+            await host.RunAsync();
         }
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+        private static void RegisterDependencies(IServiceCollection services, IConfigurationBuilder configurationBuilder)
+        {
+            var fileProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+            var configuration = configurationBuilder
+                .AddJsonFile(provider: fileProvider, path: "appsettings.json", optional: true, reloadOnChange: false)
+                .Build();
+
+            services.AddSingleton<IFileProvider>(fileProvider);
+            services.AddSingleton(configuration.GetSection("Weather").Get<WeatherConfig>());
+            services.AddSingleton<IConfiguration>(configuration);
+            services.AddSingleton<IWeatherClient, WeatherClient>();
+            services.AddSingleton<IImdbClient, ImdbClient>();
+            services.AddSingleton<IForecastService, ForecastService>();
+            services.AddSingleton<IMovieService, ImdbService>();
+            services.AddSingleton<IForecastService, ForecastService>();
+            services.AddAmbientLightSensor();
+            services.AddGeolocationSensor();
+            services.AddModalService();
+        }
 
         #endregion Methods
     }
